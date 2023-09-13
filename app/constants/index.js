c1b7c8cc2c47b921866194525a33aabcab1ba2cf
Michael Toce <mptoce@gmail.com>
2023-09-13 15:15:55 -0400

    began adding TarotCard constants info to index.js

---
+++ app/constants/index.js
@@ -0,0 +1,73 @@
+export const Cards = [
+    { MajorArcana: {
+        Fool: {
+            name: "The Fool",
+            rank: '0',
+            number: 0,
+            upright: '(Beginnings, innocence, spontaneity, opportunity  free-spirited)',
+            reversed: 'risk-taking, false-start,recklessness, holding back, apathy, ignoring an opportunity',
+            description: 'The Fool description here...',
+            rev_description: 'The Fool reversed description here...',
+            suit: 'Major',
+        },
+        Magician: {
+            name: "The Magician",
+            rank: 'I',
+            number: 1,
+            upright: '(Manifestation, willpower, success, inspiration, diplomacy, communication)',
+            reversed: 'Delays, deception, manipulation, avoidance',
+            description: 'The Magician description here...',
+            suit: 'Major',
+        },
+        HighPriestess: {
+            name: "The High Priestess",
+            rank: 'II',
+            number: 2,
+            upright: '(Intuition, divine feminine, dreams, subconcious mind)',
+            reversed: 'Secrets, withdrawal, silence, disconnection',
+            description: 'The High Priestess description here...',
+            suit: 'Major',
+        },
+        Empress: {
+            name: "The Empress",
+            rank: 'III',
+            number: 3,
+            upright: '(Femininity, fertility, beauty, nurture, abundance, peace, security, creativity)',
+            reversed: 'Creative block, dependence, imbalance, insecurity, relationships suffer',
+            description: 'The Empress description here...',
+            suit: 'Major',
+        },
+        HighPriestess: {
+            name: "The Emperor",
+            rank: 'IV',
+            number: 4,
+            upright: '(Masculinity, authority, protection, support, courage, order, structure)',
+            reversed: 'Secrets, withdrawal, silence, disconnection',
+            description: 'The High Priestess description here...',
+            suit: 'Major',
+        }
+    }},
+    { MinorArcana: {
+        Swords: {
+            Ace: {
+                name: 'Ace of Swords',
+                rank: 'Ace',
+                number: 1,
+                keywords: '(Ace of Swords keywords here...)',
+                description: 'Ace of Swords description here...',
+                suit: 'Swords',
+            }
+        },
+        Cups: {
+
+        },
+        Wands: {
+
+        },
+        Pentacles: {
+
+        }
+
+    }
+}
+]
